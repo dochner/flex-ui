6ee7b79086e4830f774b4bb78a0d666263f4cae0
Douglas Ochner <douglas.ochner@gmail.com>
2025-02-22 21:55:16 +0000

    feat: add Button

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,77 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import pkg from './package.json'
 
-// https://vite.dev/config/
+const projectRootDir = resolve(__dirname)
+
+// A bit of a hack, but lets us use the proper extension in chunk filenames
+let currentFormat = ''
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    dts({
+      tsconfigPath: 'tsconfig.build.json',
+      cleanVueFileName: true,
+      exclude: ['src/test/**', 'src/**/story/**', 'src/**/*.story.vue'],
+      rollupTypes: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(projectRootDir, 'src'),
+    },
+    dedupe: [
+      'vue',
+      '@vue/runtime-core',
+    ],
+  },
+  build: {
+    minify: false,
+    target: 'esnext',
+    sourcemap: true,
+    lib: {
+      name: '@dochner/flex-ui',
+      fileName: (format, name) => {
+        currentFormat = format
+        return `${name}.${format === 'es' ? 'js' : 'cjs'}`
+      },
+      entry: {
+        index: resolve(__dirname, 'src/index.ts'),
+      },
+    },
+    rollupOptions: {
+      external: [
+        ...Object.keys(pkg.dependencies ?? {}),
+        ...Object.keys(pkg.peerDependencies ?? {}),
+      ],
+      output: {
+        // Don't rely on preserveModules
+        // It creates a lot of unwanted files because of the multiple sections of SFC files
+        manualChunks: (moduleId, meta) => {
+          const info = meta.getModuleInfo(moduleId)
+          if (!info?.isIncluded) {
+            // Don't create empty chunks
+            return null
+          }
+
+          const [namespace, file] = moduleId.split('?')[0].split('/').slice(-2)
+          return `${namespace}/${file.slice(0, file.lastIndexOf('.'))}`
+        },
+
+        exports: 'named',
+        chunkFileNames: chunk => `${chunk.name}.${currentFormat === 'es' ? 'js' : 'cjs'}`,
+        assetFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'style.css')
+            return 'index.css'
+          return chunkInfo.name as string
+        },
+      },
+    },
+  },
 })
